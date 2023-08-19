b706495648c8006137cf2a0fe22879ca318ab699
Andrei Corovei <andrei.corovei@qubiz.com>
2023-08-19 11:44:07 +0300

    Add project files.

---
+++ lab1ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ex3
+            Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la tastaura*/
+
+            int intNumber;
+
+            Console.WriteLine("Write the number: ");
+            intNumber = int.Parse(Console.ReadLine());
+
+            int lastDigitNumber = intNumber % (10);
+            Console.WriteLine("The last digit of your number is " +  lastDigitNumber);
+        }
+    }
+}
